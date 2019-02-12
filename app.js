e3dd8194b4dd982a222408dde8205e7f04da5c47
Guilherme França <guilhermedepaula.ti@gmail.com>
2019-02-11 22:10:54 -0200

    Adicionando Bibliotecas

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+
+app.get('/tecnologia', function(req, res) {
+    res.render("sessao/tecnologia")
+});
+
+app.get('/', function(req, res) {
+    res.send('<html><body>Portal de notícias</body></html>')
+});
+
+app.listen(3000, function() {
+    console.log("Servidor rodando com Express");
+});
