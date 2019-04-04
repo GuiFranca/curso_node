ef895ef94d6c24a0658336d5d7de231de14e84c0
Guilherme França <guilhermedepaula.ti@gmail.com>
2019-04-03 21:29:04 -0300

    Aplicando validação de dados com express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,15 +1,29 @@
 module.exports = function(app){
     app.get('/formulario_inclusao_noticia', function(req,res){
-        res.render('admin/form_add_noticia');
+        res.render('admin/form_add_noticia', {validacao: {}, noticia: {}});
     });
 
     app.post('/noticias/salvar', function(req,res) {
         var noticia = req.body;
 
-        var connection = app.config.dbConnection();
-        var noticiasModel = app.app.models.noticiasModel;
+        req.assert('titulo', 'Título obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor obrigatório').notEmpty();
+        //req.assert('data_noticia', 'Data obrigatório').notEmpty().isDate({format: 'YYYY-MM-DD'});
+        req.assert('noticia', 'Noticia obrigatório').notEmpty();
 
-        noticiasModel.salvarNoticia(noticia, connection, function (error, result) {
+        var erros = req.validationErrors();
+
+        if(erros) {
+            res.render('admin/form_add_noticia', {validacao: erros, noticia: noticia});;
+            return
+        }
+
+        var connection = app.config.dbConnection();
+        var noticiasModel = new app.app.models.NoticiasDAO(connection);
+
+        noticiasModel.salvarNoticia(noticia, function (error, result) {
             res.redirect('/noticias');
         });
     });
